bbcc63f96c00f47bddbc7339aba80cb42eedfd8f
maxb00 <maxabr00@yahoo.com>
2021-07-22 16:48:53 -0400

    first draft

--- migrate.py
+++ migrate.py
@@ -8,6 +8,8 @@ from datetime import datetime
 import functools # @cache decorator will save redoing queries.
 
 FILENAME = "CSC2290_questions.json"  # TODO: Make arg.
+now = datetime.now()
+now_format = now.strftime("$d/%m/%Y %H:%M:S")
 
 # create assingment from data
 def create_assignment(connection, assingment_name, lab_id, data):
@@ -15,8 +17,8 @@ def create_assignment(connection, assingment_name, lab_id, data):
     if lang = 'java':
         query = f"""
         INSERT INTO
-          `assignments` (`name`, `description`, `java_starter`, `java_model`, `lab_id`, `published`)
-        VALUES ('{assingment_name}', 'Imported from Coding Rooms', '{starter}', '{model}', {lab_id}, 1)
+          `assignments` (`name`, `description`, `java_starter`, `java_model`, `lab_id`, `published`, `created_at`, `updated_at`)
+        VALUES ('{assingment_name}', 'Imported from Coding Rooms', '{starter}', '{model}', {lab_id}, 1, '{now_format}', '{now_format}');
         """
         execute_query(connection, query)
         problem_id = find_problem_id(connection, assingment_name, lab_id)
@@ -43,12 +45,22 @@ def create_connection(host_name, user_name, user_password):
 def create_lab(connection, course_id, lab_name):
     query = f"""
     INSERT INTO
-      `labs` (`name`, `description`, `course_id`)
-    VALUES ({lab_name}, 'Imported from Coding Rooms', {course_id});
+      `labs` (`name`, `description`, `course_id`, `created_at`, `updated_at`)
+    VALUES ('{lab_name}', 'Imported from Coding Rooms', {course_id}, '{now_format}', '{now_format}');
     """
     execute_query(connection, query)
     lab_id = find_lab_id(connection, course_id, lab_id)
     return lab_id
+
+# create test case from payload
+def create_test_case(connection, problem_id, data):
+    title, points, in, out, feedback, compare = data
+    query = f"""
+    INSERT INTO
+      `test_cases` (`title`, `assingment_id`, `input`, `output`, `points`, `compare_method`, `feedback`, `created_at`, `updated_at`)
+    VALUES ('{title}', {problem_id}, '{in}', '{out}', {points}, '{compare}', '{feedback}', '{now_format}', '{now_format}');
+    """
+    execute_query(connection, query)
 
 # my own wrapper
 @cache
@@ -145,20 +157,20 @@ def main():
         data = json.load(f)
 
     # first, we need to create the course.
-    now = datetime.now()
-    now_format = now.strftime("$d/%m/%Y %H:%M:S")
     course_name = FILENAME.split('_')[0]
     course_create_query = """
     INSERT INTO
       `courses` (`name`, `description`, `owner_id`, `created_at`, `updated_at`)
-    VALUES (%s, %s, %d, %s, %s);
+    VALUES ('%s', '%s', %d, '%s', '%s');
     """
     vals = [(course_name, "Imported from Coding Rooms",
              1237419, now_format, now_format)]
+    print('Creating course ' + course_name + '...   ', end='')
     execute_many(connection, course_create_query, vals)
 
     # after we insert, we need to find the course ID of the course we just made.
     course_id = find_course_id(connection, [(course_name, now_format)])
+    print('Complete. ID: ' + course_id)
     # then, we need to load in all of it's problems.
     labs = []
     for problem in data:
@@ -170,17 +182,41 @@ def main():
         if lab_name not in labs:
             # first time seeing this lab! let's create.
             labs.append(lab_name)
+            print("Creating Lab " + lab_name + '...   ', end='')
             lab_id = create_lab(connection, course_id, lab_name)
+            print("Complete. ID: " + lab_id)
         else:
             # hopefully, cache will speed this up.
             lab_id = find_lab_id(connection, course_id, lab_name)
 
         problem_name = split_name[1]
+        print("Creating problem " + problem_name + '...   ', end='')
         problem_data, parsed_data = parse_problem_data(problem['single_file_code_data'])
         # now that we have our data, make assignment
         problem_id = create_assignment(connection, problem_name, lab_id, problem_data)
+        print("Complete. ID: " + problem_id)
 
         # now that we've made an assingment, we must make it's test cases.
+        for tc in parsed_data['grading']['testCases']:
+            # I am going to assume the only type supported and used is stdout
+            tc_title = tc['title']
+            print("Creating test case " + tc_title + '...   ', end='')
+            tc_points = int(tc['points'])
+            tc_in = tc['stdin']
+            tc_out = tc['stdout']
+            tc_feedback = tc['feedbackOnFailure']
+            tc_compare = tc['stdoutCompareMethod']
+            # we have to parse stdoutCompareMethod further
+            # not having python 3.10 here is a BUMMER -> TODO: implement match (3.10)
+            if tc_compare = 'equals_flexible':
+                tc_compare = 'flexible'
+            else if tc_compare = 'equals':
+                tc_compare = 'exact'
+            # else, leave it and deal after, I guess. Regex is in form.
+            # we are ready for an insert
+            payload = (tc_title, tc_points, tc_in, tc_out, tc_feedback, tc_compare)
+            create_test_case(connection, problem_id, payload)
+            print('Complete.')
 
 if __name__ == '__main__':
     main()
