fcd77877619e8406ef1a1e960063cffa40a0f796
Max Barlow <barlowm15@gmail.com>
2021-10-01 11:32:04 -0400

    append newline on exact imports

--- migrate.py
+++ migrate.py
@@ -351,6 +351,7 @@ def main(due_date):
                     tc_compare = 'flexible'
                 elif tc_compare == 'equals':
                     tc_compare = 'exact'
+                    tc_out += '\n'
                 # else, leave it and deal after, I guess. Regex is in form.
                 # we are ready for an insert
                 payload = (tc_title, tc_points, tc_in,
